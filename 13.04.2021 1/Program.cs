b3cf917cce3c2ab4b210dea5adb5d7349b0b6001
unknown <you@example.c>
2021-04-13 20:16:38 +0300

    13.04.2021

---
+++ 13.04.2021 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._04._2021_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Незамаев
+            Console.WriteLine("Введите число a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число a");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число c");
+            double c = Convert.ToDouble(Console.ReadLine());
+            if ((a < b + c) & (b < a + c) & (c < a + b))
+            {
+             Console.WriteLine("Треуг.не сущ.");
+            }
+            else
+            {
+                Console.WriteLine("Треуг.не сущ.");
+            } 
+             Console.ReadKey(); 
+
+
+        }
+    }
+}
